40ef464e9ee1cf302e246d52067b680b482557e1
umasatyamahesh <umasatyamahesh.gmail.com>
2018-10-14 00:35:02 +0530

    Class Practice 10/14

---
+++ modules/app.py
@@ -0,0 +1,8 @@
+print 'this is app'
+import sys,mod
+#sys.path.append('C:\\Users\\umasa\\Desktop')
+#sys.path.insert(1,'C:\\Users\\umasa\\Desktop')
+#print sys.path
+import file1
+blockprint()
+print file1.__file__
---
+++ modules/file1.py
@@ -0,0 +1,6 @@
+print 'this is file1 from modules'
+a=2000
+b=1000
+def fun1():
+	print 'this is fun in file1'
+fun1()
