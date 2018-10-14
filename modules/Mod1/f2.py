e967f84a1e7840eacaf95520de4093977f0693a8
umasatyamahesh <umasatyamahesh.gmail.com>
2018-10-14 16:45:23 +0530

    CW 10/14

---
+++ modules/Mod1/f2.py
@@ -0,0 +1,5 @@
+def fun2():
+	print 'this is fun in f2'
+if __name__ == "__main__":
+	print 'this is f2'
+	fun2()
--- modules/app.py
+++ modules/app.py
@@ -1,8 +1,17 @@
+'''
 print 'this is app'
 import sys,mod
 #sys.path.append('C:\\Users\\umasa\\Desktop')
 #sys.path.insert(1,'C:\\Users\\umasa\\Desktop')
 #print sys.path
 import file1
-blockprint()
+#blockprint()
 print file1.__file__
+'''
+import Mod1
+#import Mod1.f1
+from Mod1 import f1,f2
+f1.fun1()
+f2.fun2()
+#Mod1.fun1()
+#Mod1.fun2()
